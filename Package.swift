5a1733edec050f56e33abf94ebc029197af3d936
PAN <panxiaoping@yonrun.com>
2020-12-09 16:02:13 +0800

    try twice

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YYText",
+            type: .dynamic,
             targets: ["YYText"]),
     ],
     dependencies: [
@@ -21,7 +22,8 @@ let package = Package(
         .target(
             name: "YYText",
             dependencies: [],
-            path: "YYText",
+            path: "Sources",
+            sources: ["YYText"],
             publicHeadersPath: "YYText"),
         .testTarget(
             name: "YYTextTests",
